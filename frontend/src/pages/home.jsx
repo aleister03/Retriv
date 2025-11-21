4bd211e8c94568f8cf77720d865f7ae02537e633
Aorko Shikder <210076494+aleister03@users.noreply.github.com>
2025-11-22 01:18:01 +0600

    Update and rename HomePage.jsx to home.jsx

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import HeroSection from '../components/home/HeroSection';
+import ServiceGrid from '../components/home/ServiceGrid';
+
+export default function HomePage() {
+  return (
+    <div>
+      <HeroSection />
+      <ServiceGrid />
+    </div>
+  );
+}
